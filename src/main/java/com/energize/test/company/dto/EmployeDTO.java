7d5370c9dbac91699a8b670bf1ad4c8e184c549d
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-08-24 15:39:38 +0400

    rename fields

--- src/main/java/com/energize/test/company/dto/EmployeDTO.java
+++
@@ -1,8 +0,0 @@
-package com.energize.test.company.dto;
-
-public class EmployeDTO {
-
-    private String EmployeName;
-
-    private String EmployeSurname;
-}
---
+++ src/main/java/com/energize/test/company/dto/EmployeeDTO.java
@@ -0,0 +1,24 @@
+package com.energize.test.company.dto;
+
+public class EmployeeDTO {
+
+    private String employeeName;
+
+    private String employeeSurname;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeSurname() {
+        return employeeSurname;
+    }
+
+    public void setEmployeeSurname(String employeeSurname) {
+        this.employeeSurname = employeeSurname;
+    }
+}
